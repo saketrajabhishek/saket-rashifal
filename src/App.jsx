19952832c92f9804e8e18158d1bd52c21792772e
saketrajabhishek <saketrajabhishek@gmail.com>
2025-01-12 01:57:57 +0530

    add analytics and speed insights components to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import Footer from "./components/Footer";
 import ThemeProvider from "./components/ThemeProvider";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <>
       <ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
         <Header />
         <Home />
         <Footer />
